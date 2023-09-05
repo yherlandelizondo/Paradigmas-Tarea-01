722fc252919ce6d2371c41c49e70d5bb2d0cde8f
yherlandelizondo <yherland25@gmail.com>
2023-09-04 23:10:15 -0600

    creating the writeFile function

--- testLogic.rkt
+++ testLogic.rkt
@@ -9,3 +9,27 @@
 
 (provide displayNewNode displayEdge)
 
+
+(define (writeFile graph edges)
+  #|
+    create the .txt file; if exist, replace it.
+  |#
+  (define output-port (open-output-file "./tmp/temp.txt" #:exists 'replace))
+
+  #|
+    adding the file content
+  |#
+  (write (list graph edges) output-port)
+
+  #|
+    close the file
+  |#
+  (close-output-port output-port))
+
+
+#|
+  testing
+|#
+(writeFile '((a (a b c)) (a (e f g)) (b (1 2 3)) (c ()))
+           '((Hola mundo mi nombre es hola))
+           )
