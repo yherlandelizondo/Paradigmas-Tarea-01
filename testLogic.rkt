b641e35f217d3dc9aa3ea465734b5f96f41d57fb
yherlandelizondo <yherland25@gmail.com>
2023-09-04 23:55:17 -0600

    creating the readFunction

--- testLogic.rkt
+++ testLogic.rkt
@@ -9,7 +9,9 @@
 
 (provide displayNewNode displayEdge)
 
-
+#|
+  Function to write data to a .txt file
+|#
 (define (writeFile graph edges)
   #|
     create the .txt file; if exist, replace it.
@@ -26,6 +28,19 @@
   |#
   (close-output-port output-port))
 
+#|
+  Function for reading data from a file
+|#
+(define (readFile)
+  #|
+    (open-input-file "./tmp/temp.txt") -> store the file descriptor (number to refer to an open file in the OS)
+    (read (open-input-file "./tmp/temp.txt")) -> read the file, using the file descriptor
+    (close-input-port (open-input-file "./tmp/temp.txt")) -> close the open file
+  |#
+  (close-input-port (open-input-file "./tmp/temp.txt")) (read (open-input-file "./tmp/temp.txt")))
+
+
+
 
 #|
   testing
@@ -33,3 +48,5 @@
 (writeFile '((a (a b c)) (a (e f g)) (b (1 2 3)) (c ()))
            '((Hola mundo mi nombre es hola))
            )
+
+(display (readFile))
